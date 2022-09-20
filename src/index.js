14a7d279a8695ced00cef7656dcda2e95d896781
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2022-09-20 11:38:28 -0700

    added header

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>
 );
 
 reportWebVitals();
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/components/Header.js",
   ],
   theme: {
     extend: {},
