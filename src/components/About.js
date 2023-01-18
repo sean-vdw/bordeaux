c2146124ec021d35e595ca3c838c9e5e02aac1a1
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2023-01-18 11:55:51 -0800

    adjusted responsiveness for hero and about sections

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,15 @@
 
 export default function About() {
   return (
-    <div id="about" className="bg-transparent relative my-20">
-      <div className="mx-auto max-w-md px-6 sm:max-w-6xl lg:max-w-7xl lg:px-8">
-        <div className="rounded-3xl bg-gradient-to-l drop-shadow-2xl from-sky-800 to-cyan-700 py-10 px-6 sm:py-16 sm:px-12 lg:flex lg:items-center lg:py-20 lg:px-20">
-          <div className="lg:w-0 lg:flex-1 text-center">
+    <div id="about" className="bg-transparent relative my-20 py-10">
+      <div className="mx-auto w-11/12 lg:w-10/12 2xl:w-8/12 lg:px-8 border-y border-gray-700">
+        <div className="py-10 px-6 sm:py-16 sm:px-12 lg:flex lg:items-center lg:py-20 lg:px-20">
+          <div className="lg:w-0 lg:flex-1 text-center items-center">
             <h2 className="text-lg font-semibold text-gray-900 uppercase">About Us</h2>
-            <p className="mt-1 text-4xl font-bold tracking-tight text-white sm:text-5xl lg:text-4xl">
+            <p className="mt-1 text-2xl font-bold tracking-tight text-slate-600 sm:text-4xl">
               Your Personal CFO
             </p>
-            <p className="mt-4 text-xl text-gray-100 leading-relaxed">
+            <p className="mt-4 text-xl text-gray-700 leading-relaxed">
               With over $4 billion in assets under management, we act as collaborative thought partners that build resilient portfolios, manage our clients' lives and legacies, and provide deep diligence on tax and estate strategy. 
               In doing so, we believe families can create leverage to benefit the people and causes they care about most.
             </p>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -240,10 +240,10 @@ export default function Header() {
       </div>
       <main>
         <div className="relative px-6 lg:px-8 h-screen">
-          <div className="flex justify-end max-w-3xl pt-20 mt-32 pb-20 sm:pt-48 sm:pb-40">
+          <div className="flex justify-end max-w-3xl 2xl:max-w-4xl pt-20 mt-32 pb-20 sm:pt-48 sm:pb-40">
             <div>
               <div>
-                <h1 className="text-4xl font-light tracking-tight text-left sm:text-6xl 2xl:text-8xl">
+                <h1 className="text-4xl font-light tracking-tight text-left sm:text-6xl 2xl:text-7xl 2xl:mb-12">
                   Premier family office services for <TextTransition springConfig={presets.default}>{clients[index % clients.length]}.</TextTransition>
                 </h1>
                 <div className="mt-8 flex gap-x-4 sm:justify-start">
