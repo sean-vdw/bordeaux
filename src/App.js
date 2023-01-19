38a1baa59127e49b90ea8f4f603fa82af02545fd
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2023-01-19 11:24:58 -0800

    added animated cursor

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { Route } from 'react-router-dom';
+import AnimatedCursor from 'react-animated-cursor';
 
 // COMPONENTS
 import Hero from './components/Hero';
@@ -11,6 +12,17 @@ import Cta from './components/Cta';
 function App() {
   return (
     <div className="App">
+      <AnimatedCursor
+        color="12,74,110"
+        innerSize={8}
+        outerSize={28}
+        innerScale={1}
+        outerScale={1.7}
+        outerAlpha={0}
+        outerStyle={{
+          border: '2px solid rgb(148 163 184)'
+        }}
+      />
       <Hero/>
       <About />
       <Services />
